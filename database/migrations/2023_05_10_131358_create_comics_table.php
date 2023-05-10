92db161ec87c462263abc245a7d796456c79520d
DavideAtzo <atzo9477@gmail.com>
2023-05-10 15:26:24 +0200

    migrazione tabella comics

---
+++ database/migrations/2023_05_10_131358_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 5, 2);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('type', 25);
+            $table->string('artists', 40);
+            $table->string('writers', 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
