a463d2c369c65d49c6dfa8a10b2cb9f97da8055f
DavideAtzo <atzo9477@gmail.com>
2023-05-10 16:04:34 +0200

    problema con il seeders

--- database/migrations/2023_05_10_131358_create_comics_table.php
+++ database/migrations/2023_05_10_131358_create_comics_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->text('description');
             $table->string('thumb');
             $table->float('price', 5, 2);
-            $table->string('series', 30);
+            $table->string('series', 40);
             $table->date('sale_date');
             $table->string('type', 25);
             $table->string('artists', 40);
